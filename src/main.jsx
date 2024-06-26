449a969c3421f97bb5c173e6e07efe53a814baf8
Matias Laime <matulai1221@gmail.com>
2024-06-26 17:37:06 -0300

    agregado de ruteo para las paginas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import QuestionsPage from './pages/QuestionsPage';
+import ResultsPage from './pages/ResultsPage';
 import ReactDOM from 'react-dom/client';
-import StartPage from '@pages/StartPage';
 
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <StartPage />,
+  },
+  {
+    path: '/questions/:difficulty',
+    element: <QuestionsPage />,
+  },
+  {
+    path: '/results/:questionsIds/:answers',
+    element: <ResultsPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <StartPage />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
